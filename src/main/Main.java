97ce4d92a0258f67e765054d48248891597168d1
Kirill150 <kirilsbelajevs@gmail.com>
2016-08-07 11:32:40 +0300

    Dobavil pakedzi i sgruppiroval klassi po nim

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,14 +1,9 @@
-package sample;
+package main;
 
 import javafx.application.Application;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
-import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.ChoiceBox;
-import javafx.scene.control.TableView;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -17,7 +12,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("../FXML/ui/Main.fxml"));
         primaryStage.setTitle("Liepajas Tramvajs");
         primaryStage.setScene(new Scene(root, 640, 480));
         primaryStage.show();
